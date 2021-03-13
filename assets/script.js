8162c3894147b9d4ef48b47e664c88960899a3ce
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-03-13 10:19:55 -0500

    added some js vars and to dos

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,17 @@
 var teamA = document.querySelector("teamA");
 var teamB = document.querySelector("teamB");
+var gameTime = document.querySelector("");
+var gameLocation = document.querySelector("");
+
+
+// function to call next game time and location
+    //find time of next game
+    //compare time to current time
+    //append time to html
+    //find location of game
+    // append location
+    // save location for map api
+
+
+
+
