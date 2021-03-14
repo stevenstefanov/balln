55c4557a92afe934bf277c296934909ea030686c
Lou Falla <lou3892@gmail.com>
2021-03-14 12:09:01 -0400

    ajax call

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,34 @@
+
+
+/*Variables */
 var teamA = document.querySelector("teamA");
 var teamB = document.querySelector("teamB");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Ticketmaster API*/
+$.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events/G5diZfkn0B-bh.json?apikey=wkeUKU7nGwu8KaAr6CW0pqhy3LP5gh4f",
+    async:true,
+    dataType: "json",
+    success: function(json) {
+                console.log(json);
+                var futureGames = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
+             },
+    error: function(xhr, status, err) {
+                // This time, we do not end up here!
+             }
+  });
+ 
