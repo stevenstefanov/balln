0ceeb905a9cbf98123c6ba81ad5462ed262e3e27
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-03-16 22:57:19 -0400

    added team name sto search, api query results

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,47 @@
 var searchFormEl = document.querySelector("#search-form");
+var teamEl = document.querySelector("#team-dropdown");
+
 
 function handleSearchFormSubmit(event) {
   event.preventDefault();
-  var teamsDropdownVal = document.querySelector(‘#teams-dropdown’).value;
+  var teamsDropdownVal = teamEl.value;
   if (!teamsDropdownVal) {
-    console.error(‘You need a search input value!‘);
+    console.error('You need a search input value!');
     return;
   }
-  var queryString = ‘./search-results.html?team=’ + teamsDropdownVal;
+  var queryString = './results.html?team=' + teamsDropdownVal;
   location.assign(queryString);
 }
+
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+fetch("https://api-basketball.p.rapidapi.com/teams?league=12&season=2020-2021", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "a02badb577msh53c62c24f7e4112p157b9bjsne248c865880a",
+		"x-rapidapi-host": "api-basketball.p.rapidapi.com"
+	}
+})
+.then(response => {
+	if (response.ok) {
+        response.json().then(function (data) {
+            console.log(data);
+            displayTeamData(data);
+        });
+    }
+})
+.catch(err => {
+	console.error(err);
+});
+
+function displayTeamData(data) {
+    for (var i = 0; i < data.response.length; i++) {
+        var teamName = data.response[i].name;
+        console.log(teamName);
+        console.log(data.response[i].id);
+        var teamTitle = document.createElement('option');
+        teamTitle.textContent = teamName;
+        teamTitle.value = teamName.toLowerCase();
+        teamEl.appendChild(teamTitle);  
+    }   
+}
