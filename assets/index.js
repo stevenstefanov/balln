ab859d64561ffb906164e85d2b9934e92da3baf7
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-03-19 21:13:16 -0400

    added comments to index.js

--- assets/index.js
+++ assets/index.js
@@ -2,33 +2,33 @@
 var searchFormEl = document.querySelector("#search-form");
 var teamEl = document.querySelector("#team-dropdown");
 
-// function to set localStorage
+// Function to set localStorage
 function setLocalStorage () {
   var userChoice = teamEl.value;
-
   localStorage.setItem("userChoice",userChoice)
   return userChoice;
 }
 
-// Function to handly submit button
+// Function to handle submit button
 function handleSearchFormSubmit(event) {
   event.preventDefault();
   var teamsDropdownVal = teamEl.value;
+
   if (!teamsDropdownVal) {
-    console.error('You need a search input value!');
+    console.error("You need a search input value!");
     return;
   }
-  var queryString = './results.html?team=' + teamsDropdownVal;
+
+  var queryString = "./results.html?team=" + teamsDropdownVal;
   location.assign(queryString);
   setLocalStorage();
 }
 
+searchFormEl.addEventListener("submit", handleSearchFormSubmit);
 
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
+// Fetch that gets 2020-2021 season team information
 fetch("https://api-basketball.p.rapidapi.com/teams?league=12&season=2020-2021", {
-	"method": "GET",
+  "method": "GET",
 	"headers": {
 		"x-rapidapi-key": "a02badb577msh53c62c24f7e4112p157b9bjsne248c865880a",
 		"x-rapidapi-host": "api-basketball.p.rapidapi.com"
@@ -36,25 +36,25 @@ fetch("https://api-basketball.p.rapidapi.com/teams?league=12&season=2020-2021", 
 })
 .then(response => {
 	if (response.ok) {
-        response.json().then(function (data) {
-            console.log(data);
-            displayTeamData(data);
-        });
-    }
+    response.json().then(function(data) {
+      console.log(data);
+      displayTeamData(data);
+    });
+  }
 })
 .catch(err => {
 	console.error(err);
 });
 
-// For loop which populates team names in drop down menu
+// Function with for loop which populates team names in drop down menu
 function displayTeamData(data) {
-    for (var i = 0; i < data.response.length-2; i++) {
-        var teamName = data.response[i].name;
-        console.log(teamName);
-        console.log(data.response[i].id);
-        var teamTitle = document.createElement('option');
-        teamTitle.textContent = teamName;
-        teamTitle.value = teamName.toLowerCase();
-        teamEl.appendChild(teamTitle);  
-    }   
+  for (var i = 0; i < data.response.length-2; i++) {
+    var teamName = data.response[i].name;
+    console.log(teamName);
+    console.log(data.response[i].id);
+    var teamTitle = document.createElement("option");
+    teamTitle.textContent = teamName;
+    teamTitle.value = teamName.toLowerCase();
+    teamEl.appendChild(teamTitle);  
+  }   
 }
